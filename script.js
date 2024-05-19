d1af405205a9e5181aaa256dd0968e24f57f133f
fabricionetto <agostinhonetto73@gmail.com>
2024-05-18 22:26:19 -0300

    Tratamento de error

--- script.js
+++ script.js
@@ -16,9 +16,13 @@
 }
 
 async function exibirUserName(id) {
-     const user = await getUser(id)
 
-     console.log(`O nome do usuario é: ${user.data.first_name}`)
+    try {
+        const user = await getUser(id)
+        console.log(`O nome do usuario é: ${user.data.first_name}`)
+    } catch (error) {
+        console.log(`Error: ${err}`)
+    }
 }
 
 exibirUserName(3)
